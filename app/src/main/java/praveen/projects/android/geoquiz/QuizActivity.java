4deaf3d3c264053f3876791f2a775a3560cc2614
Praveen Mathanagopal <praveen.tapedlive@gmail.com>
2015-10-24 19:26:26 -0600

    UI setup
    buttons set and checked with toast

--- app/src/main/java/praveen/projects/android/geoquiz/QuizActivity.java
+++ app/src/main/java/praveen/projects/android/geoquiz/QuizActivity.java
@@ -8,24 +8,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private Button bTrueButton;
+    private Button bFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bTrueButton = (Button)findViewById(R.id.true_button);
+        bFalseButton = (Button)findViewById(R.id.false_button);
+        bTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.true_button,Toast.LENGTH_SHORT).show();
             }
         });
+
+        bFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.false_button,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
