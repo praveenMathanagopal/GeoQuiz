c2f98157e43c557269efd126f2a94135ff7aef03
Praveen Mathanagopal <praveen.tapedlive@gmail.com>
2015-10-25 19:34:04 -0600

    save bundle
    save information while rotating

--- app/src/main/java/praveen/projects/android/geoquiz/QuizActivity.java
+++ app/src/main/java/praveen/projects/android/geoquiz/QuizActivity.java
@@ -1,10 +1,12 @@
 package praveen.projects.android.geoquiz;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +17,9 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     private Button bTrueButton;
-    private Button bFalseButton;
     private Button bNextButton;
+    private Button bPrevButton;
+    private Button bFalseButton;
     private TextView tQuestion_View;
     private int mCurrentIndex;
     private Question cQues;
@@ -25,14 +28,16 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        mCurrentIndex = 0;
+        if (savedInstanceState != null)
+            mCurrentIndex = savedInstanceState.getInt("QNO");
+        else
+            mCurrentIndex = 0;
         bTrueButton = (Button) findViewById(R.id.true_button);
         bFalseButton = (Button) findViewById(R.id.false_button);
         bNextButton = (Button) findViewById(R.id.next_button);
+        bPrevButton = (Button) findViewById(R.id.prev_button);
         tQuestion_View = (TextView) findViewById(R.id.question_text_box);
-
-        cQues = QuestionBank.qBank.get(mCurrentIndex);
-        tQuestion_View.setText(cQues.getqId());
+        updateQuestion(0);
         bTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,12 +55,17 @@ public class QuizActivity extends AppCompatActivity {
         bNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex++;
-                mCurrentIndex = mCurrentIndex % QuestionBank.qBank.size();
-                tQuestion_View.setText(QuestionBank.qBank.get(mCurrentIndex).getqId());
+                updateQuestion(1);
+
             }
         });
 
+        bPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion(-1);
+            }
+        });
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -70,7 +80,20 @@ public class QuizActivity extends AppCompatActivity {
 //        });
     }
 
-    private void checkAnswer(boolean selected){
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("QNO", mCurrentIndex);
+    }
+
+    private void updateQuestion(int increment) {
+        Log.d("MYTAG", "in update question");
+        mCurrentIndex += increment + QuestionBank.qBank.size();
+        mCurrentIndex = mCurrentIndex % QuestionBank.qBank.size();
+        tQuestion_View.setText(QuestionBank.qBank.get(mCurrentIndex).getqId());
+    }
+
+    private void checkAnswer(boolean selected) {
         if (QuestionBank.qBank.get(mCurrentIndex).getQans() == selected)
             Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
         else
